0d652e40e5ad5945d44db6eedad2b90492275b99
Arnau Caballero <2024_arnau.caballero@iticbcn.cat>
2026-02-17 16:04:38 +0100

    ou

--- auditoria.sh
+++ auditoria.sh
@@ -6,3 +6,23 @@ ldapsearch -x -H "$server_ldap" -D "cn=admin,dc=tecnosolutions,dc=lan" -w admin 
 numero_usuaris=$(grep -cve "^\s*$" informe_data.txt)
 echo "Recompte total d'usuaris al sistema es de $numero_usuaris" > informe_data.txt
 cat informe_data.txt
+ldapsearch -x -H "$server_ldap" -D "cn=admin,dc=tecnosolutions,dc=lan" -w admin -LLL -b "ou=comptabilitat,ou=usuaris,$base_dn" objectclass=posixaccount dn > informe_data.txt
+numero_usuaris=$(grep -cve "^\s*$" informe_data.txt)
+echo "Recompte total d'usuaris al sistema es de $numero_usuaris" > informe_data.txt
+cat informe_data.txt
+ldapsearch -x -H "$server_ldap" -D "cn=admin,dc=tecnosolutions,dc=lan" -w admin -LLL -b "ou=direccio,ou=usuaris,$base_dn" objectclass=posixaccount dn > informe_data.txt
+numero_usuaris=$(grep -cve "^\s*$" informe_data.txt)
+echo "Recompte total d'usuaris al sistema es de $numero_usuaris" > informe_data.txt
+cat informe_data.txt
+ldapsearch -x -H "$server_ldap" -D "cn=admin,dc=tecnosolutions,dc=lan" -w admin -LLL -b "ou=marketing,ou=usuaris,$base_dn" objectclass=posixaccount dn > informe_data.txt
+numero_usuaris=$(grep -cve "^\s*$" informe_data.txt)
+echo "Recompte total d'usuaris al sistema es de $numero_usuaris" > informe_data.txt
+cat informe_data.txt
+ldapsearch -x -H "$server_ldap" -D "cn=admin,dc=tecnosolutions,dc=lan" -w admin -LLL -b "ou=sistemes,ou=usuaris,$base_dn" objectclass=posixaccount dn > informe_data.txt
+numero_usuaris=$(grep -cve "^\s*$" informe_data.txt)
+echo "Recompte total d'usuaris al sistema es de $numero_usuaris" > informe_data.txt
+cat informe_data.txt
+ldapsearch -x -H "$server_ldap" -D "cn=admin,dc=tecnosolutions,dc=lan" -w admin -LLL -b "ou=vendes,ou=usuaris,$base_dn" objectclass=posixaccount dn > informe_data.txt
+numero_usuaris=$(grep -cve "^\s*$" informe_data.txt)
+echo "Recompte total d'usuaris al sistema es de $numero_usuaris" > informe_data.txt
+cat informe_data.txt
